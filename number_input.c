4f9cbca4c27acccd9e1a0e13b4b6a27665157958
diegorego514-ship-it <diegorego514@gmail.com>
2025-12-02 17:02:26 -0300

    Add files via upload

---
+++ number_input.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+int number;
+printf("input number: ");
+scanf("%d", &number);
+if (number >= 100) {
+    printf("Number is greater:\n ");
+
+
+
+int number;
+printf("input number: ");
+scanf("%d", &number);
+if (number >= 50);
+    printf("Number is less:\n");
+    
+number;
+printf("input number: ");
+scanf("%d", &number);
+if (number >= 105);
+    printf("number is equal to 100:\n ");
+}
+}
