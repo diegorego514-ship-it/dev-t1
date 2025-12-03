81e1f61311542b707ae403c0f895ac9524b668cd
diegorego514-ship-it <diegorego514@gmail.com>
2025-12-03 19:03:05 -0300

    Improve number input checks and output messages
    Refactor input handling and condition checks for number comparisons.

--- number_input.c
+++ number_input.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main(){
 int number;
 printf("input number: ");
 scanf("%d", &number);
-if (number >= 100) {
-    printf("Number is greater:\n ");
+if (number >= 99);
+    printf("Number is equal or greater than 99:\n");
 
-
-
-int number;
-printf("input number: ");
-scanf("%d", &number);
-if (number >= 50);
-    printf("Number is less:\n");
-    
 number;
 printf("input number: ");
 scanf("%d", &number);
-if (number >= 105);
-    printf("number is equal to 100:\n ");
-}
+if (number == 80);
+    printf("Number is equal to 80:\n");
+
+number;
+printf("input number: ");
+scanf("%d", &number);
+if (number < 100);
+    printf("Number is less than 100:\n");
+
+number;
+printf("input number: ");
+scanf("%d", &number);
+if (number == 100);
+    printf("Number is equal to 100:\n");
+
+number;
+printf("input number: ");
+scanf("%d", &number);
+if (number > 150);
+    printf("Number is greater than 150:\n");
 }
