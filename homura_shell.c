659e0eb30312c253c404ed298f83df13c20e196a
diegorego514-ship-it <diegorego514@gmail.com>
2025-12-04 14:19:43 -0300

    Add files via upload

---
+++ homura_shell.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main(){
+
+int shell;
+printf("input the kind of shell program do you want to create: ");
+scanf("%d", &shell);
+if (shell);
+// printf("input the name of the program that you want to create:") 
+}
